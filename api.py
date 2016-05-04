7f58e4624222046cd6ac417b1ab3f7522df7d1e3
Yana Kesala <radiantMoxie@users.noreply.github.com>
2016-05-03 18:41:38 -0700

    feat: Consolidate to one guess property

--- api.py
+++ api.py
@@ -93,32 +93,34 @@ class HangmanApi(remote.Service):
         #Handle illegal moves
         if len(request.guess) != 1:
           return game.to_form('Please only enter a single letter.')
-        elif request.guess in game.wrong_guesses or request.guess in game.correct_guesses:
+        elif request.guess in game.guesses:
           return game.to_form('You have already guessed ' + request.guess + '! Please guess a new letter.')
         elif request.guess not in "abcdefghijklmnopqrstuvwxyz":
           return game.to_form('Only letters are allowed as guesses!')
 
         game.attempts += 1
+        game.guesses += request.guess
 
         if request.guess in game.target_word:
-            game.correct_guesses += request.guess
-
-            # Replace underscores in word_so_far with correctly guessed letters
+            # Replace hyphens in word_so_far with correctly guessed letters
             for i in range(len(game.target_word)):
-              if game.target_word[i] in game.correct_guesses:
-                game.word_so_far = game.word_so_far[:i] + game.target_word[i] + game.word_so_far[i+1:]
-
+                if game.target_word[i] in game.guesses:
+                    game.word_so_far = game.word_so_far[:i] + game.target_word[i] + game.word_so_far[i+1:]
             msg = ('You guessed correctly!')
             game.put()
             return game.to_form(msg)
         else:
-            game.wrong_guesses += request.guess
-            if len(game.wrong_guesses) > 5:
-              return game.to_form("You lose!The word was " + game.target_word)
+            #Calculate number of incorrect guesses
+            incorrect_guesses = 0
+            for i in range(len(game.guesses)):
+                if game.guesses[i] not in game.target_word:
+                    incorrect_guesses += 1
+            if incorrect_guesses > 5:
+                return game.to_form("You lose!The word was " + game.target_word)
             else:
-              msg = ('Incorrect guess! Letter ' + request.guess + ' is not in the word. You are ' + str((6 - len(game.wrong_guesses))) + ' wrong guess(es) from HANGMAN.')
-              game.put()
-              return game.to_form(msg)
+                msg = ('Incorrect guess! Letter ' + request.guess + ' is not in the word. You are ' + str(6 - incorrect_guesses) + ' incorrect guess(es) from HANGMAN.')
+                game.put()
+                return game.to_form(msg)
 
 
     @endpoints.method(response_message=ScoreForms,
