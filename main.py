c8f6bf5b8b081e93501803aeacc3a3035ba12d76
Yana Kesala <radiantMoxie@users.noreply.github.com>
2016-05-11 14:45:42 -0700

    feat: update cron job
    Email users with unfinished games every 24 hours with a reminder

--- main.py
+++ main.py
@@ -6,36 +6,28 @@ import logging
 
 import webapp2
 from google.appengine.api import mail, app_identity
-from api import GuessANumberApi
+from api import HangmanApi
 
-from models import User
-
+from models import User, Game
 
 class SendReminderEmail(webapp2.RequestHandler):
     def get(self):
-        """Send a reminder email to each User with an email about games.
-        Called every hour using a cron job"""
+        """Send a reminder email to each User with at least 1 incomplete game.
+        Called every 24 hours using a cron job"""
         app_id = app_identity.get_application_id()
         users = User.query(User.email != None)
         for user in users:
-            subject = 'This is a reminder!'
-            body = 'Hello {}, try out Guess A Number!'.format(user.name)
-            # This will send test emails, the arguments to send_mail are:
-            # from, to, subject, body
-            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+            games = Game.query(Game.user == user.key)
+            games = games.filter(Game.game_over == False)
+            if games.count() > 0:
+                subject = 'This is a reminder to prevent an execution!'
+                body = 'Hello {}, please complete your Hangman game!'.format(user.name)
+
+                mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
                            user.email,
                            subject,
                            body)
 
-
-class UpdateAverageMovesRemaining(webapp2.RequestHandler):
-    def post(self):
-        """Update game listing announcement in memcache."""
-        GuessANumberApi._cache_average_attempts()
-        self.response.set_status(204)
-
-
 app = webapp2.WSGIApplication([
     ('/crons/send_reminder', SendReminderEmail),
-    ('/tasks/cache_average_attempts', UpdateAverageMovesRemaining),
 ], debug=True)
